ade07e8e634713d31fbfe8052be965003e6d5bd3
Janos Dobronszki <dobronszki@gmail.com>
2020-10-30 11:37:07 +0100

    [WIP] Comments service

--- blog/comments/handler/comments.go
+++ blog/comments/handler/comments.go
@@ -3,14 +3,41 @@ package handler
 import (
 	"context"
 
-	"github.com/micro/micro/v3/service/logger"
+	"github.com/google/uuid"
+	"github.com/micro/dev/model"
+	"github.com/micro/micro/v3/service/store"
 	pb "github.com/micro/services/blog/comments/proto"
 )
 
-type Comments struct{}
+type Comments struct {
+	comments  model.Table
+	idIndex   model.Index
+	postIndex model.Index
+}
 
-// Call is a single request handler called via client.Call or the generated client code
-func (c *Comments) Save(ctx context.Context, req *pb.Request, rsp *pb.Response) error {
-	logger.Info("Not yet implemented")
-	return nil
+func NewComments() *Comments {
+	postIndex := model.ByEquality("post")
+	postIndex.Order.Type = model.OrderTypeDesc
+
+	idIndex := model.ByEquality("id")
+	idIndex.Order.Type = model.OrderTypeUnordered
+
+	return &Comments{
+		comments:  model.NewTable(store.DefaultStore, "users", model.Indexes(postIndex), nil),
+		postIndex: postIndex,
+		idIndex:   idIndex,
+	}
+}
+
+func (c *Comments) New(ctx context.Context, req *pb.NewRequest, rsp *pb.NewResponse) error {
+	return c.comments.Save(pb.Comment{
+		Id:      uuid.New().String(),
+		Post:    req.Post,
+		Author:  req.Author,
+		Message: req.Message,
+	})
+}
+
+func (c *Comments) List(ctx context.Context, req *pb.ListRequest, rsp *pb.ListResponse) error {
+	return c.comments.List(c.postIndex.ToQuery(req.Post), rsp.Comments)
 }
