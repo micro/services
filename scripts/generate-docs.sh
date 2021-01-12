8008fb78edf2433ff36704eb8bbe1fb6dcc105cc
Janos Dobronszki <dobronszki@gmail.com>
2021-01-12 15:07:58 +0000

    Fix gen

--- scripts/generate-docs.sh
+++ scripts/generate-docs.sh
@@ -1,6 +1,5 @@
 mkdir docs
-contentFolder=../docs/hugo-tania/exampleSite/content/post
-mkdir $contentFolder
+mkdir ./docs/hugo-tania/exampleSite/content/post
 echo "services.m3o.com" > docs/CNAME
 dir=$(pwd)
 for d in */; do
@@ -8,10 +7,13 @@ for d in */; do
     echo $d
     cd $d
     serviceName=${d//\//}
+    contentFolder=../docs/hugo-tania/exampleSite/content/post
     timeout 3s make proto || continue
     echo "Copying html for $serviceName"
+    pwd
+    touch $contentFolder/$serviceName.md
     echo "---\ntitle: $servicename\n---\n" > $contentFolder/$serviceName.md
-    cat README.md > $contentFolder/$serviceName.md || continue
+    cat README.md >> $contentFolder/$serviceName.md || continue
     cp redoc-static.html ../docs/$serviceName-api.html || continue
 done
 pwd
