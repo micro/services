0b045486195f4400223b6335cf65a5e0b338ca93
Janos Dobronszki <dobronszki@gmail.com>
2021-01-28 07:22:14 +0000

    Fixes

--- datastore/handler/datastore.go
+++ datastore/handler/datastore.go
@@ -3,12 +3,23 @@ package handler
 import (
 	"context"
 	"encoding/json"
+	"strings"
 
 	log "github.com/micro/micro/v3/service/logger"
 	"github.com/micro/micro/v3/service/model"
 
 	datastore "github.com/micro/services/datastore/proto"
 )
+
+var indexIndex = model.Index{
+	FieldName: "TypeOf",
+}
+
+type IndexRecord struct {
+	ID     string
+	TypeOf string
+	Index  model.Index
+}
 
 type Datastore struct {
 }
@@ -20,8 +31,14 @@ func (e *Datastore) Create(ctx context.Context, req *datastore.CreateRequest, rs
 	if err != nil {
 		return err
 	}
-
-	return model.New(map[string]interface{}{}, nil).Context(ctx).Create(m)
+	indexes, err := e.getIndexes(ctx)
+	if err != nil {
+		return err
+	}
+	db := model.New(map[string]interface{}{}, &model.Options{
+		Indexes: indexes,
+	})
+	return db.Context(ctx).Create(m)
 }
 
 func (e *Datastore) Update(ctx context.Context, req *datastore.UpdateRequest, rsp *datastore.UpdateResponse) error {
@@ -31,8 +48,30 @@ func (e *Datastore) Update(ctx context.Context, req *datastore.UpdateRequest, rs
 	if err != nil {
 		return err
 	}
+	indexes, err := e.getIndexes(ctx)
+	if err != nil {
+		return err
+	}
+	db := model.New(map[string]interface{}{}, &model.Options{
+		Indexes: indexes,
+	})
+	return db.Context(ctx).Update(m)
+}
 
-	return model.New(map[string]interface{}{}, nil).Context(ctx).Update(m)
+func (e *Datastore) getIndexes(ctx context.Context) ([]model.Index, error) {
+	indexDb := model.New(map[string]interface{}{}, &model.Options{
+		Indexes: []model.Index{indexIndex},
+	})
+	result := []IndexRecord{}
+	err := indexDb.Context(ctx).Read(model.QueryEquals("TypeOf", "_index"), &result)
+	if err != nil {
+		return nil, err
+	}
+	indexes := []model.Index{}
+	for _, v := range result {
+		indexes = append(indexes, v.Index)
+	}
+	return indexes, nil
 }
 
 func (e *Datastore) Read(ctx context.Context, req *datastore.ReadRequest, rsp *datastore.ReadResponse) error {
@@ -52,7 +91,14 @@ func (e *Datastore) List(ctx context.Context, req *datastore.ListRequest, rsp *d
 	log.Info("Received Datastore.List request")
 	q := toQuery(req.Query)
 	result := []map[string]interface{}{}
-	err := model.New(map[string]interface{}{}, nil).Context(ctx).Read(q, &result)
+	indexes, err := e.getIndexes(ctx)
+	if err != nil {
+		return err
+	}
+	db := model.New(map[string]interface{}{}, &model.Options{
+		Indexes: indexes,
+	})
+	err = db.Context(ctx).Read(q, &result)
 	if err != nil {
 		return err
 	}
@@ -63,8 +109,17 @@ func (e *Datastore) List(ctx context.Context, req *datastore.ListRequest, rsp *d
 
 func (e *Datastore) CreateIndex(ctx context.Context, req *datastore.CreateIndexRequest, rsp *datastore.CreateIndexResponse) error {
 	log.Info("Received Datastore.Index request")
-	q := toQuery(req.Query)
-	return model.New(map[string]interface{}{}, nil).Context(ctx).Delete(q)
+
+	index := toIndex(req.Index)
+	indexRecord := IndexRecord{
+		ID:     index.FieldName + index.Type + index.Order.FieldName + string(index.Order.Type),
+		Index:  index,
+		TypeOf: "_index",
+	}
+	db := model.New(map[string]interface{}{}, &model.Options{
+		Indexes: []model.Index{indexIndex},
+	})
+	return db.Context(ctx).Create(indexRecord)
 }
 
 func (e *Datastore) Delete(ctx context.Context, req *datastore.DeleteRequest, rsp *datastore.DeleteResponse) error {
@@ -80,4 +135,19 @@ func toQuery(pquery *datastore.Query) model.Query {
 		q.Order.Type = model.OrderType(pquery.Order.OrderType.String())
 	}
 	return q
+}
+
+func toIndex(pindex *datastore.Index) model.Index {
+	i := model.Index{
+		FieldName: pindex.FieldName,
+		Type:      pindex.Type,
+		Unique:    pindex.Unique,
+	}
+	if pindex.Order != nil {
+		i.Order = model.Order{
+			FieldName: pindex.FieldName,
+			Type:      model.OrderType(strings.ToLower(pindex.Order.OrderType.String())),
+		}
+	}
+	return i
 }
