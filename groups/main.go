85cff12269e8e7afcdfe7d87ff387a6c1ed90511
Ben Toogood <bentoogood@gmail.com>
2021-02-12 14:40:15 +0000

    Debugging

--- groups/main.go
+++ groups/main.go
@@ -35,7 +35,7 @@ func main() {
 	}
 
 	// Register handler
-	pb.RegisterGroupsHandler(srv.Server(), &handler.Groups{DB: db})
+	pb.RegisterGroupsHandler(srv.Server(), &handler.Groups{DB: db.Debug()})
 
 	// Run service
 	if err := srv.Run(); err != nil {
