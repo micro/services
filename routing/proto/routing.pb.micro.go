b1587aa5f7ab6114bec4d39fdf7e8663cafcf293
Dominic Wong <dom@m3o.com>
2021-03-18 15:52:44 +0000

    docs

--- routing/proto/routing.pb.micro.go
+++ routing/proto/routing.pb.micro.go
@@ -6,7 +6,7 @@ package routing
 import (
 	fmt "fmt"
 	proto "github.com/golang/protobuf/proto"
-	_ "github.com/golang/protobuf/ptypes/wrappers"
+	_ "google.golang.org/protobuf/types/known/wrapperspb"
 	math "math"
 )
 
