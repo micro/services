be35a50daff8d7a19b3f08c9352bce89fd6136fb
Janos Dobronszki <dobronszki@gmail.com>
2021-04-29 14:49:11 +0100

    upload

--- image/handler/image.go
+++ image/handler/image.go
@@ -17,6 +17,45 @@ import (
 type Image struct{}
 
 func (e *Image) Upload(ctx context.Context, req *img.UploadRequest, rsp *img.UploadResponse) error {
+	var srcImage image.Image
+	if len(req.Base64) > 0 {
+		res := []byte{}
+		_, err := base64.StdEncoding.Decode([]byte(req.Base64), res)
+		if err != nil {
+			return err
+		}
+		srcImage, err = png.Decode(bytes.NewReader(res))
+		if err != nil {
+			return err
+		}
+	} else {
+		response, err := http.Get(req.Url)
+		if err != nil {
+			return err
+		}
+		srcImage, err = jpeg.Decode(response.Body)
+		if err != nil {
+			return err
+		}
+		defer response.Body.Close()
+	}
+	buf := new(bytes.Buffer)
+	err := jpeg.Encode(buf, srcImage, nil)
+	if err != nil {
+		return err
+	}
+	if req.OutputURL {
+		err = store.DefaultBlobStore.Write(req.ImageID, buf)
+		if err != nil {
+			return err
+		}
+		return nil
+	} else {
+		dst := []byte{}
+		base64.StdEncoding.Encode(dst, buf.Bytes())
+		rsp.Base64 = string(dst)
+		return nil
+	}
 	return nil
 }
 
@@ -43,17 +82,22 @@ func (e *Image) Resize(ctx context.Context, req *img.ResizeRequest, rsp *img.Res
 		}
 		defer response.Body.Close()
 	}
-	resultImage := imaging.Resize(srcImage, 128, 128, imaging.Lanczos)
+	resultImage := imaging.Resize(srcImage, int(req.Width), int(req.Height), imaging.Lanczos)
+	buf := new(bytes.Buffer)
+	err := jpeg.Encode(buf, resultImage, nil)
+	if err != nil {
+		return err
+	}
 	if req.OutputURL {
-		buf := new(bytes.Buffer)
-		err := jpeg.Encode(buf, resultImage, nil)
-		if err != nil {
-			return err
-		}
 		err = store.DefaultBlobStore.Write(req.ImageID, buf)
 		if err != nil {
 			return err
 		}
+		return nil
+	} else {
+		dst := []byte{}
+		base64.StdEncoding.Encode(dst, buf.Bytes())
+		rsp.Base64 = string(dst)
 		return nil
 	}
 	return nil
