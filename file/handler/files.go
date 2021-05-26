36f6509c547ae359b64629a0ef7bef0740e7ceef
Janos Dobronszki <dobronszki@gmail.com>
2021-05-26 14:29:53 +0100

    F

--- file/handler/files.go
+++ file/handler/files.go
@@ -56,7 +56,7 @@ func (e *File) Read(ctx context.Context, req *file.ReadRequest, rsp *file.ReadRe
 
 	// filter the file
 	for _, file := range files {
-		if file.Path == req.Path && file.Name == req.Name {
+		if req.Path != "" && strings.HasPrefix(file.Path, req.Path) {
 			// strip the tenant id
 			file.Project = strings.TrimPrefix(file.Project, tenantId+"/")
 			file.Path = strings.TrimPrefix(file.Path, req.Project+"/")
