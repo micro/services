046bb80121e1564b7b9420c596306a429eb6ea7d
Janos Dobronszki <dobronszki@gmail.com>
2021-06-21 09:13:59 +0100

    F

--- user/handler/handler.go
+++ user/handler/handler.go
@@ -211,5 +211,6 @@ func (s *User) SendVerificationEmail(ctx context.Context, req *pb.SendVerificati
 	if err != nil {
 		return err
 	}
-	return s.domain.SendEmail(req.Email, users[0].Username, req.Subject, req.TextContent, token, req.RedirectUrl)
+
+	return s.domain.SendEmail(req.FromName, req.Email, users[0].Username, req.Subject, req.TextContent, token, req.RedirectUrl)
 }
