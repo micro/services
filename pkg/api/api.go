b6b3d837174dfe8412fd473fcebd2a61ae4c5905
Asim Aslam <asim@aslam.me>
2021-08-27 12:45:35 +0100

    add api package

---
+++ pkg/api/api.go
@@ -0,0 +1,28 @@
+// Package api is a helper for calling external third party apis
+package api
+
+import (
+	"fmt"
+	"net/http"
+	"encoding/json"
+	"io/ioutil"
+)
+
+func Get(url string, rsp interface{}) error {
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode != 200 {
+		return fmt.Errorf("Non 200 response %v: %v", resp.StatusCode, string(b))
+	}
+
+	return json.Unmarshal(b, rsp)
+}
