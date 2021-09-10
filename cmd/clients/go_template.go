e3f12370b9df187692e38feb38355eeaeb877603
Janos Dobronszki <dobronszki@gmail.com>
2021-09-10 14:11:06 +0100

    F

--- cmd/clients/go_template.go
+++ cmd/clients/go_template.go
@@ -70,3 +70,8 @@ import(
 	fmt.Println(rsp, err)
 }
 `
+
+const curlExampleTemplate = `curl "https://api.m3o.com/v1/{{ $service.Name }/{{ title .endpoint }}" \
+-H "Content-Type: application/json" \
+-H "Authorization: Bearer $MICRO_API_TOKEN" \
+-d '{{ tsExampleRequest $service.Name .endpoint $service.Spec.Components.Schemas .example.Request }}'`
