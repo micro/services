6df66fc1908dd705190303c91edfae45162329d4
Dominic Wong <dom@m3o.com>
2021-09-15 17:01:01 +0100

    QR code API

---
+++ qr/README.md
@@ -0,0 +1,6 @@
+Quickly generate QR Codes with a single call
+
+# QR Code Service
+
+Generate QR codes for whatever you like. Typically used for website URLs but could be any text like emails, phone numbers, addresses etc.
+
---
+++ qr/handler/qr.go
@@ -0,0 +1,86 @@
+package handler
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/micro/micro/v3/service/config"
+	"github.com/micro/micro/v3/service/errors"
+	log "github.com/micro/micro/v3/service/logger"
+	"github.com/micro/micro/v3/service/store"
+	"github.com/micro/services/pkg/tenant"
+	qr "github.com/micro/services/qr/proto"
+	"github.com/skip2/go-qrcode"
+)
+
+const (
+	prefixByTenant = "qrByTenant"
+)
+
+type QrCode struct {
+	Filename string `json:"filename"`
+	Created  int64  `json:"created"`
+	Text     string `json:"text"`
+}
+
+type Qr struct {
+	cdnPrefix string
+}
+
+func New() *Qr {
+	v, err := config.Get("micro.qr.cdnprefix")
+	if err != nil {
+		log.Fatalf("Failed to get CDN prefix %s", err)
+	}
+	pref := v.String("")
+	if len(pref) == 0 {
+		log.Fatalf("Failed to get CDN prefix")
+	}
+	return &Qr{cdnPrefix: pref}
+}
+
+func (q *Qr) Generate(ctx context.Context, request *qr.GenerateRequest, response *qr.GenerateResponse) error {
+	if len(request.Text) == 0 {
+		return errors.BadRequest("qr.generate", "Missing parameter text")
+	}
+	ten, ok := tenant.FromContext(ctx)
+	if !ok {
+		log.Errorf("Error retrieving tenant")
+		return errors.Unauthorized("qr.generate", "Unauthorized")
+	}
+	qrc, err := qrcode.Encode(request.Text, qrcode.Medium, 256)
+	if err != nil {
+		log.Errorf("Error generating QR code %s", err)
+		return errors.InternalServerError("qr.generate", "Error while generating QR code")
+	}
+
+	fileName := fmt.Sprintf("%s.png", uuid.New().String())
+	if err := store.DefaultBlobStore.Write(
+		fileName, bytes.NewBuffer(qrc),
+		store.BlobContentType("image/png"),
+		store.BlobPublic(true),
+		store.BlobNamespace(ten)); err != nil {
+		log.Errorf("Error saving QR code to blob store %s", err)
+		return errors.InternalServerError("qr.generate", "Error while generating QR code")
+	}
+	// store record of it
+	rec := QrCode{
+		Filename: fileName,
+		Created:  time.Now().Unix(),
+		Text:     request.Text,
+	}
+	b, _ := json.Marshal(&rec)
+	if err := store.Write(&store.Record{
+		Key:   fmt.Sprintf("%s/%s/%s", prefixByTenant, ten, fileName),
+		Value: b,
+	}); err != nil {
+		log.Errorf("Error saving QR code record %s", err)
+		return errors.InternalServerError("qr.generate", "Error while generating QR code")
+	}
+	response.Qr = fmt.Sprintf("%s/micro/qr/%s/%s", q.cdnPrefix, ten, rec.Filename)
+	return nil
+}
