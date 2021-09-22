77c32acee83cbdd982e347f8552b4aa0810d01f8
asim <asim@users.noreply.github.com>
2021-09-22 19:20:19 +0000

    Commit from GitHub Actions (Publish APIs & Clients)

--- examples/prayer/times/node/prayerTimes.js
+++ examples/prayer/times/node/prayerTimes.js
@@ -1,6 +1,6 @@
 import * as prayer from "m3o/prayer";
 
-//
+// Get the prayer (salah) times for a location on a given date
 async function PrayerTimes() {
   let prayerService = new prayer.PrayerService(process.env.MICRO_API_TOKEN);
   let rsp = await prayerService.times({
