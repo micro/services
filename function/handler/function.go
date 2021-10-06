150fc52d88b0b457f6c353efee06f53a00c90c4a
Janos Dobronszki <dobronszki@gmail.com>
2021-10-06 14:41:41 +0100

    F

--- function/handler/function.go
+++ function/handler/function.go
@@ -130,6 +130,9 @@ func (e *Function) Call(ctx context.Context, req *function.CallRequest, rsp *fun
 	fmt.Println("URL:>", url)
 
 	js, _ := json.Marshal(req.Request)
+	if len(req.Request.Fields) == 0 {
+		js = []byte("{}")
+	}
 	r, err := http.NewRequest("POST", url, bytes.NewBuffer(js))
 	if err != nil {
 		return err
