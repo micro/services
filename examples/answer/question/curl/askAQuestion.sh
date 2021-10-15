351352f5cf3cda46cdc89713dc702051a635e8db
crufter <crufter@users.noreply.github.com>
2021-10-15 10:22:09 +0000

    Commit from GitHub Actions (Publish APIs & Clients)

--- examples/answer/question/curl/askAQuestion.sh
+++ examples/answer/question/curl/askAQuestion.sh
@@ -2,5 +2,5 @@ curl "https://api.m3o.com/v1/answer/Question" \
 -H "Content-Type: application/json" \
 -H "Authorization: Bearer $MICRO_API_TOKEN" \
 -d '{
-  "query": "google"
+  "query": "microsoft"
 }'
