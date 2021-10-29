44a434d80cf4687b3a0666058495064e569c94a1
asim <asim@users.noreply.github.com>
2021-10-29 12:42:57 +0000

    Commit from GitHub Actions (Generate Clients & Examples)

---
+++ examples/notes/subscribe/go/subscribeToEvents.go
@@ -0,0 +1,17 @@
+package example
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/micro/services/clients/go/notes"
+)
+
+// Specify the note to events
+func SubscribeToEvents() {
+	notesService := notes.NewNotesService(os.Getenv("MICRO_API_TOKEN"))
+	rsp, err := notesService.Subscribe(&notes.SubscribeRequest{
+		Id: "63c0cdf8-2121-11ec-a881-0242e36f037a",
+	})
+	fmt.Println(rsp, err)
+}
