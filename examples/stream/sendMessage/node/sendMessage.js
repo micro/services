71e26f92c3337fa72de3fa2ecf4d0b9c79ba6f14
asim <asim@users.noreply.github.com>
2021-11-03 15:28:08 +0000

    Commit from GitHub Actions (Generate Clients & Examples)

---
+++ examples/stream/sendMessage/node/sendMessage.js
@@ -0,0 +1,13 @@
+const { StreamService } = require("m3o/stream");
+
+// SendMessage a message to the stream.
+async function sendMessage() {
+  let streamService = new StreamService(process.env.MICRO_API_TOKEN);
+  let rsp = await streamService.sendMessage({
+    channel: "general",
+    text: "Hey checkout this tweet https://twitter.com/m3oservices/status/1455291054295498752",
+  });
+  console.log(rsp);
+}
+
+sendMessage();
