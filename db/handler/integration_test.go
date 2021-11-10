0d3ee19760a513995aa64043a17510374ce17eb4
Janos Dobronszki <dobronszki@gmail.com>
2021-11-10 16:08:35 +0000

    F

--- db/handler/integration_test.go
+++ db/handler/integration_test.go
@@ -24,25 +24,108 @@ func getHandler(t *testing.T) *Db {
 	return h
 }
 
-func TestCreate(t *testing.T) {
+func TestBasic(t *testing.T) {
 	h := getHandler(t)
-	record, _ := json.Marshal(map[string]interface{}{
-		"name":     "Jane",
-		"age":      42,
-		"isActive": true,
-		"id":       "1",
+	ctx := auth.ContextWithAccount(context.Background(), &auth.Account{Issuer: "basic_test", ID: "test"})
+	rs := []map[string]interface{}{
+		{
+			"name":     "Jane",
+			"age":      42,
+			"isActive": true,
+			"id":       "1",
+		},
+		{
+			"name":     "Joe",
+			"age":      112,
+			"isActive": false,
+			"id":       "2",
+		},
+	}
+	for _, v := range rs {
+		record, _ := json.Marshal(v)
+		rec := &structpb.Struct{}
+		err := rec.UnmarshalJSON(record)
+		if err != nil {
+			t.Fatal(err)
+		}
+		err = h.Create(ctx, &db.CreateRequest{
+			Table:  "users",
+			Record: rec,
+		}, &db.CreateResponse{})
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	t.Run("number ==", func(t *testing.T) {
+		readRsp := &db.ReadResponse{}
+		err := h.Read(ctx, &db.ReadRequest{
+			Table: "users",
+			Query: "age == 112",
+		}, readRsp)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if len(readRsp.Records) != 1 || readRsp.Records[0].AsMap()["id"].(string) != "2" {
+			t.Fatal(readRsp)
+		}
 	})
-	rec := &structpb.Struct{}
-	ctx := auth.ContextWithAccount(context.Background(), &auth.Account{Issuer: "test", ID: "test"})
-	err := rec.UnmarshalJSON(record)
-	if err != nil {
-		t.Fatal(err)
-	}
-	err = h.Create(ctx, &db.CreateRequest{
-		Table:  "users",
-		Record: rec,
-	}, &db.CreateResponse{})
-	if err != nil {
-		t.Fatal(err)
-	}
+
+	t.Run("bool ==", func(t *testing.T) {
+		readRsp := &db.ReadResponse{}
+		err := h.Read(ctx, &db.ReadRequest{
+			Table: "users",
+			Query: "active == false",
+		}, readRsp)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if len(readRsp.Records) != 1 || readRsp.Records[0].AsMap()["id"].(string) != "2" {
+			t.Fatal(readRsp)
+		}
+	})
+
+	t.Run("string ==", func(t *testing.T) {
+		readRsp := &db.ReadResponse{}
+		err := h.Read(ctx, &db.ReadRequest{
+			Table: "users",
+			Query: "name == 'Jane'",
+		}, readRsp)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if len(readRsp.Records) != 1 || readRsp.Records[0].AsMap()["id"].(string) != "1" {
+			t.Fatal(readRsp)
+		}
+	})
+
+	t.Run("order number asc", func(t *testing.T) {
+		readRsp := &db.ReadResponse{}
+		err := h.Read(ctx, &db.ReadRequest{
+			Table:   "users",
+			OrderBy: "age",
+			Order:   "asc",
+		}, readRsp)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if len(readRsp.Records) != 2 || readRsp.Records[0].AsMap()["id"].(string) != "2" || readRsp.Records[1].AsMap()["id"].(string) != "1" {
+			t.Fatal(readRsp)
+		}
+	})
+
+	t.Run("order number desc", func(t *testing.T) {
+		readRsp := &db.ReadResponse{}
+		err := h.Read(ctx, &db.ReadRequest{
+			Table:   "users",
+			OrderBy: "age",
+			Order:   "desc",
+		}, readRsp)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if len(readRsp.Records) != 2 || readRsp.Records[0].AsMap()["id"].(string) != "1" || readRsp.Records[1].AsMap()["id"].(string) != "2" {
+			t.Fatal(readRsp)
+		}
+	})
 }
