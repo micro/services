cf0993ed7ba6f1367d16295b4379011913878589
hezhaoyang <gmzhaoyang@gmail.com>
2021-12-13 19:18:34 +0800

    feat: replace model with store

--- rss/main.go
+++ rss/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"time"
+
 	"github.com/micro/micro/v3/service"
 	"github.com/micro/micro/v3/service/logger"
+	"github.com/micro/micro/v3/service/store"
+
 	"github.com/micro/services/pkg/tracing"
 	"github.com/micro/services/rss/handler"
 	pb "github.com/micro/services/rss/proto"
@@ -14,8 +18,21 @@ func main() {
 		service.Name("rss"),
 	)
 
+	st := store.DefaultStore
+	crawl := handler.NewCrawl(st)
+	rss := handler.NewRss(st, crawl)
+
+	// crawl
+	go func() {
+		crawl.FetchAll()
+		tick := time.NewTicker(1 * time.Minute)
+		for _ = range tick.C {
+			crawl.FetchAll()
+		}
+	}()
+
 	// Register handler
-	pb.RegisterRssHandler(srv.Server(), handler.NewRss())
+	pb.RegisterRssHandler(srv.Server(), rss)
 	traceCloser := tracing.SetupOpentracing("rss")
 	defer traceCloser.Close()
 
