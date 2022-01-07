86c5ceafcd90bc5954adaea7b2ef728dc3d95401
Asim Aslam <asim@aslam.me>
2022-01-07 11:51:18 +0000

    Add the carbon api

---
+++ carbon/domain/domain.go
@@ -0,0 +1,21 @@
+package domain
+
+type Project struct {
+	Name string `json:"name"`
+	Percentage float64 `json:"splitPercentage"`
+	Tonnes float64 `json:"splitAmountTonnes"`
+}
+
+type OffsetRequest struct {
+	Number int32  `json:"number"`
+	Units  string `json:"units"`
+}
+
+type OffsetResponse struct {
+	Number   int32   `json:"number"`
+	Units    string  `json:"units"`
+	Tonnes   float64 `json:"numberInTonnes"`
+	Amount   float64   `json:"amount"`
+	Currency string  `json:"currency"`
+	Projects []Project `json:"projectDetails"`
+}
