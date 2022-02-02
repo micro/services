c8ae03f9b2a4ab930323859fcc141c91006c69e5
Dominic Wong <dom@m3o.com>
2022-02-02 16:28:24 +0000

    search delete

--- search/main.go
+++ search/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/micro/micro/v3/service"
 	"github.com/micro/micro/v3/service/logger"
+	admin "github.com/micro/services/pkg/service/proto"
 	"github.com/micro/services/search/handler"
 	pb "github.com/micro/services/search/proto"
 )
@@ -14,8 +15,10 @@ func main() {
 		service.Version("latest"),
 	)
 
+	h := handler.New(srv)
 	// Register handler
-	pb.RegisterSearchHandler(srv.Server(), handler.New(srv))
+	pb.RegisterSearchHandler(srv.Server(), h)
+	admin.RegisterAdminHandler(srv.Server(), h)
 
 	// Run service
 	if err := srv.Run(); err != nil {
