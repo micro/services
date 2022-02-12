e61ae7805d41641e4cdc724dd438a17b55d4a1d5
Daniel Joudat <lambdaR@users.noreply.github.com>
2022-02-12 22:54:31 +0300

    fix validPostUserData part 2 and users/Update

--- user/domain/domain.go
+++ user/domain/domain.go
@@ -425,6 +425,10 @@ func (domain *Domain) Read(ctx context.Context, userId string) (*user.Account, e
 	return domain.readUserByKey(ctx, generateAccountStoreKey(ctx, userId))
 }
 
+func (domain *Domain) SearchByUserId(ctx context.Context, userId string) (*user.Account, error) {
+	return domain.ReadUserByKey(ctx, generateAccountStoreKey(ctx, userId))
+}
+
 func (domain *Domain) SearchByUsername(ctx context.Context, username string) (*user.Account, error) {
 	return domain.readUserByKey(ctx, generateAccountUsernameStoreKey(ctx, username))
 }
